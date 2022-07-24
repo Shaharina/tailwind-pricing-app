ed689afaa3b46686508bf7c3ffa037b3b4a8e588
Shaharina Mita <S.mita90232@gmail.com>
2022-07-25 01:02:16 +0600

    header added

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './Navbar/Navbar';
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h2>aaaaaaaa</h2>
+            <p>gbggh</p>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/Components/Header/Navbar/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Navbar = () => {
+    const routes = [{ id: 1, name: 'shop' }];
+    return (
+        <div>
+            <ul>
+                <li>
+                    <a href='#'></a>
+                </li>
+                <li>
+                    <a href=''></a>
+                </li>
+                <li>
+                    <a href=''></a>
+                </li>
+                <li>
+                    <a href=''></a>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default Navbar;
