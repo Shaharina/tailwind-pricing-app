d7d736353ed38fdc820ac270e5051b453e69a56a
Shaharina Mita <S.mita90232@gmail.com>
2022-07-27 00:02:00 +0600

    style added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/Header';
+import Pricing from './Components/Header/Pricing/Pricing';
 
 function App() {
     return (
         <div className='App'>
             <Header></Header>
             <h1 className='text-6xl'>welcome</h1>
+            <Pricing></Pricing>
         </div>
     );
 }
--- src/Components/Header/Navbar/Navbar.js
+++ src/Components/Header/Navbar/Navbar.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from '../../../Link/Link';
+import { MenuIcon, XIcon } from '@heroicons/react/solid';
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false);
     const routes = [
         { id: 1, name: 'Home', link: '/home' },
         { id: 2, name: 'Shop', link: '/shop' },
         { id: 3, name: 'Deals', link: '/deals' },
         { id: 4, name: 'Coupons', link: '/coupons' },
+        { id: 5, name: 'Contact', link: '/contact' },
     ];
     return (
-        <nav>
-            <ul className='md:flex justify-center '>
+        <nav className='bg-indigo-200'>
+            <div onClick={() => setOpen(!open)} className='w-6 h-6'>
+                {open ? <XIcon></XIcon> : <MenuIcon></MenuIcon>}
+            </div>
+            <ul className={`md:flex justify-center md:static bg-indigo-200 w-full text-blue-600 absolute duration-500 ease-in ${open ? 'top-6' : 'top-[-120px] '}`}>
                 {routes.map(route => (
                     <Link key={route.id} route={route}></Link>
                 ))}
